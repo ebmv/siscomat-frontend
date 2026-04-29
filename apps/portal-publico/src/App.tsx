10e3a34c68d5636dafb736d0f493f9a0cdaeff4c
elda <ebmv03@gmail.com>
2026-04-29 14:10:38 -0700

    Añade Input y Button en shared-ui

--- apps/portal-publico/src/App.tsx
+++ apps/portal-publico/src/App.tsx
@@ -1,16 +1,26 @@
-// Test componente
-import { TestCard } from "@siscomat/shared-ui";
-import "./index.css";
+import { Input } from "@siscomat/shared-ui";
+import { Button } from "@siscomat/shared-ui";
 
 function App() {
   return (
-    <div className="min-h-screen bg-light-3 p-10 flex flex-col items-center gap-6">
-      <h1 className="heading-1">Portal público</h1>
-      {/* Test Card */}
-      <TestCard
-        title="¡Monorepo funciona!"
-        description="Esta tarjeta viene directamente desde shared-ui. Si Tailwind está bien configurado, verás esto con diseño, bordes y sombras."
-      />
+    <div className="min-h-screen bg-white p-10 flex flex-col items-center gap-6">
+      <h1 className="heading-1">Input/Button showcase (?)</h1>
+
+      <div className="w-80 flex flex-col gap-4">
+        {/* Inputs */}
+        <Input label="Folio" placeholder="Ej. 2026-1234-56" />
+        <Input label="Correo" placeholder="ola@ejemplo.com" />
+        <Input label="Correo" value="texto inválido" error="Correo inválido" onChange={() => {}} />
+        <Input label="Campo deshabilitado" value="Zainul Anshor" disabled />
+
+        {/* Buttons primary */}
+        <Button label="Default" onClick={() => {}} />
+        <Button label="Disabled" disabled />
+
+        {/* Buttons secondary */}
+        <Button label="Default" variant="secondary" onClick={() => {}} />
+        <Button label="Disabled" variant="secondary" disabled />
+      </div>
     </div>
   );
 }
