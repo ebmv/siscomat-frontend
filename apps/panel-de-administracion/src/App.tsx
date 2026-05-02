f10e4226ec602d85b60ba1547ef370a4fec42dfe
jessica <jessymontano@hotmail.com>
2026-05-01 21:07:48 -0700

    fix prevent authenticated user from accessing login page

--- apps/panel-de-administracion/src/App.tsx
+++ apps/panel-de-administracion/src/App.tsx
@@ -1,24 +1,35 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Login, Home } from "./pages";
-import { Navbar, AuthProvider, ProtectedRoute } from "./components";
+import {
+  Navbar,
+  AuthProvider,
+  ProtectedRoute,
+  PublicRoute,
+} from "./components";
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/login" element={<Login />} />
+        <div className="flex flex-col min-h-screen bg-light-2">
+          <Navbar />
+          <main className="flex-1 w-full flex flex-col">
+            <Routes>
+              <Route element={<PublicRoute />}>
+                <Route path="/login" element={<Login />} />
+              </Route>
 
-          <Route element={<ProtectedRoute />}>
-            <Route path="/" element={<Home />} />
-            // TODO: agregar rutas para plantillas y constancias
-          </Route>
+              <Route element={<ProtectedRoute />}>
+                <Route path="/" element={<Home />} />
+                // TODO: agregar rutas para plantillas y constancias
+              </Route>
 
-          <Route element={<ProtectedRoute requiresAdmin={true} />}>
-            // TODO: agregar ruta para gestion de usuarios
-          </Route>
-        </Routes>
+              <Route element={<ProtectedRoute requiresAdmin={true} />}>
+                // TODO: agregar ruta para gestion de usuarios
+              </Route>
+            </Routes>
+          </main>
+        </div>
       </BrowserRouter>
     </AuthProvider>
   );
--- apps/panel-de-administracion/src/components/auth/index.ts
+++ apps/panel-de-administracion/src/components/auth/index.ts
@@ -1,2 +1,3 @@
 export * from "./AuthContext";
 export * from "./ProtectedRoute";
+export * from "./PublicRoute";
